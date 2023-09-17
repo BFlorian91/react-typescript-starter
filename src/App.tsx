73180626d14a758ee512bee859c296194a810361
BFlorian91 <b.florian91@icloud.com>
2023-09-17 13:37:50 +0200

    add preview for the readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is a <code>src/starter.ts</code>
+        </p>
+        <p>
+          With{' '}
+          <strong>
+            <i>Typescript, jest, eslint, prettier, husky</i>
+          </strong>{' '}
+          full configured
         </p>
         <a
           className="App-link"
@@ -20,7 +27,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
